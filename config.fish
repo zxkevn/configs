3a8bbabc0c581e7fb239ed6cb12488ea9666ca0c
dropkick <1975987+dropk1ck@users.noreply.github.com>
2016-08-19 20:09:35 -0400

    config.fish updates

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
 set -x PYENV_ROOT "$HOME/.pyenv"
-set -x PATH $PYENV_ROOT/bin $PATH
+set -x RBENV_ROOT "$HOME/.rbenv"
+set -x PATH $PYENV_ROOT/bin $RBENV_ROOT/bin $PATH
 set -x PATH $PATH ~/bin
 set -x PYTHONPATH $PYTHONPATH ~/code
 status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (rbenv init -|psub)
