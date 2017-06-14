9f5c81fb838127356a5eec382580222465098d90
dropkick <1975987+dropk1ck@users.noreply.github.com>
2017-06-14 02:04:32 +0000

    fish stuff

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ mkdir -p ~/.config/fish
 ln -f -s $PWD/config.fish ~/.config/fish/
 ln -f -s $PWD/.tmux.conf ~
 ln -f -s $PWD/.vimrc ~
+mkdir -p ~/.config/fish/functions
+ln -f -s $PWD/fish_prompt.fish ~/.config/fish/functions/
+ln -f -s $PWD/fish_user_key_bindings.fish ~/.config/fish/functions/
