8862f819877b5dddf31a4cce222c58928ce525ed
dropkick <1975987+dropk1ck@users.noreply.github.com>
2018-02-25 15:02:16 -0500

    changes

--- install.sh
+++ install.sh
@@ -2,12 +2,9 @@
 
 set -ex
 PWD=`pwd`
-ln -f -s $PWD/.bash_profile ~
-ln -f -s $PWD/.bashrc ~
 mkdir -p ~/.config/fish
 ln -f -s $PWD/config.fish ~/.config/fish/
 ln -f -s $PWD/.tmux.conf ~
 ln -f -s $PWD/.vimrc ~
 mkdir -p ~/.config/fish/functions
-ln -f -s $PWD/fish_prompt.fish ~/.config/fish/functions/
 ln -f -s $PWD/fish_user_key_bindings.fish ~/.config/fish/functions/
